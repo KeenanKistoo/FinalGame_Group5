e1519262aa2f5a6aa2226fbb8fba1862ce671936
KeenanKistoo <118082735+KeenanKistoo@users.noreply.github.com>
2023-10-11 15:26:49 +0200

    Added weight text
    under the guns in the shop it now shows it

--- FinalGame_Group5/Assets/keenan/scripts_keenan/WeaponController/PlayerWeightManager.cs
+++ FinalGame_Group5/Assets/keenan/scripts_keenan/WeaponController/PlayerWeightManager.cs
@@ -12,16 +12,22 @@ public class PlayerWeightManager : MonoBehaviour
     private PlayerGear _gear;
     [SerializeField]
     private PlayerMovement _player;
+    public WeaponAttach attach;
 
     private void Start()
     {
        // _playerWeight = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerWeight>();
         _player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerMovement>();
+        _gear = GameObject.FindGameObjectWithTag("inventory").GetComponent<PlayerGear>();
     }
 
     public void WeightChange(float speed)
     {
         _player.normSpeed = speed;
+    }
+
+    public void WeightMaxChange(int max)
+    {
     }
 
 }
