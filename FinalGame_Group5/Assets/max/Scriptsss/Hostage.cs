96767dd57e99166a27c3137448ddbc60c7d77ccb
MaxKruger123 <maxkruger321@gmail.com>
2023-11-11 17:02:08 +0200

    Finished basic hostage setup

--- FinalGame_Group5/Assets/max/Scriptsss/Hostage.cs
+++ FinalGame_Group5/Assets/max/Scriptsss/Hostage.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Hostage : MonoBehaviour
 {
     public GameObject HostageText;
+    public Animator animator;
+    public Image fillImage;
+    public bool canRescue;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,15 +18,43 @@ public class Hostage : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (fillImage.fillAmount > 0.95f)
+        {
+            Debug.Log(fillImage.fillAmount);
+            GameObject UI = GameObject.Find("HostageRescue");
+            UI.SetActive(false);
+            gameObject.SetActive(false);
+        }
+        if (canRescue)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                animator.SetBool("Fill", true);
+                if (fillImage.fillAmount > 0.1f)
+                {
+                    Debug.Log(fillImage.fillAmount);
+                    GameObject UI = GameObject.Find("HostageRescue");
+                    UI.SetActive(false);
+                    gameObject.SetActive(false);
+                }
+            }
+
+            if(Input.GetKeyUp(KeyCode.F))
+            {
+                animator.SetBool("Fill", false);
+                
+            }
+        }
     }
 
     public void OnTriggerEnter(Collider collision)
     {
 
-        if (collision.collider.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             HostageText.SetActive(true);
+            canRescue = true;
         }
         
     }
@@ -30,11 +62,13 @@ public class Hostage : MonoBehaviour
     public void OnTriggerExit(Collider collision)
     {
 
-        if (collision.collider.tag == "Player")
+        if (collision.gameObject.tag == "Player")
         {
             HostageText.SetActive(false);
+            canRescue = false;
         }
         
     }
-            
+
+
 }
